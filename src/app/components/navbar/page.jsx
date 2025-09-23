a5877e12c6135258bed008668e1e64efcb4199f4
deepakgithub11 <deepakboranaoffice.com>
2025-09-23 06:55:29 +0530

    add/navbar

--- src/app/components/navbar/page.jsx
+++ src/app/components/navbar/page.jsx
@@ -1,8 +1,33 @@
+'use client'
 import React from 'react'
+import Link from 'next/link'
+import './style.css'
 
 const Navbar = () => {
+
+
   return (
-    <div>Navbar</div>
+    <>
+      <nav className='container'>
+        <div className='navbar'>
+          <div className='nav-logo'>
+            <img src='images/bird.png' width={50} height={50} />
+            <ul className='nav-link' id='nav-link'>
+              <li><Link href={{ pathname: '/' }}> Home </Link></li>
+              <li><Link href={{ pathname: '/contact' }}> About Us</Link></li>
+              <li><Link href={{ pathname: '/contact' }}>Contact Us</Link></li>
+            </ul>
+          </div>
+          <ul className='nav-link' id='nav-link'>
+
+            <li><Link href={{ pathname: '/login' }}>Login</Link></li>
+            <li><Link href={{ pathname: '/sign-up' }}>Sign Up</Link></li>
+          </ul>
+         
+        </div >
+      </nav >
+      
+    </>
   )
 }
 
